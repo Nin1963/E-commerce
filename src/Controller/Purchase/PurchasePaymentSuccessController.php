70bb2a4d63bdeb321deca6df108efd12e98e54c5
Nin1963 <ninayamina1963@gmail.com>
2021-10-15 12:02:24 +0200

    Mise en olace des évènements

--- src/Controller/Purchase/PurchasePaymentSuccessController.php
+++ src/Controller/Purchase/PurchasePaymentSuccessController.php
@@ -4,18 +4,21 @@ namespace App\Controller\Purchase;
 
 use App\Entity\Purchase;
 use App\Cart\CartService;
+use App\Event\PurchaseSuccessEvent;
 use App\Repository\PurchaseRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
+
 
 class PurchasePaymentSuccessController extends AbstractController {
     /**
      * @Route("/purchase/terminate/{id}", name="purchase_payment_success")
      * @IsGranted("ROLE_USER")
      */
-    public function success($id, PurchaseRepository $purchaseRepository, EntityManagerInterface $em, CartService $cartService) {
+    public function success($id, PurchaseRepository $purchaseRepository, EntityManagerInterface $em, CartService $cartService, EventDispatcherInterface $dispatcher) {
         // 1. Je récupère la commande
         $purchase = $purchaseRepository->find($id);
 
@@ -31,6 +34,10 @@ class PurchasePaymentSuccessController extends AbstractController {
 
         // 3 . Je vide le panier
         $cartService->empty();
+
+        // Lancer un évènement qui permette aux autres développeurs de réagir à la prise d'une commande
+        $purchaseEvent = new PurchaseSuccessEvent($purchase);
+        $dispatcher->dispatch($purchaseEvent, 'purchase.success');
 
         // 4 . Je redirige avec un flash vers la liste de commandes
         $this->addFlash('success', "La commande a été confirmée et payée !");
